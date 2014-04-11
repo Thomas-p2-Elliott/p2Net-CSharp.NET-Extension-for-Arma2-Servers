9619afc16c720f225d9644b7e5e0270da8b13a86
ZombZ <player2@zombz.net>
2014-04-11 20:54:58 +0100

    Created UpdateGit Bat
    Just for me :)

---
+++ updateGit.bat
@@ -0,0 +1,7 @@
+:: Copies from: 	X:\_Scripting\p2ArmaNet\build4\p2Net1
+:: To: 				C:\Users\Player2\Documents\GitHub\p2Net
+
+
+XCOPY "X:\_Scripting\p2ArmaNet\build4\p2Net1" "C:\Users\Player2\Documents\GitHub\p2Net" /E /V /F /Y
+
+timeout 10
